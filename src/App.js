51749fc2b9ad09a347d3cb2facbf0a3bc49471c4
Amornath <amor2017nstucste@gmail.com>
2022-01-11 22:39:31 +0600

    code format changes

--- src/App.js
+++ src/App.js
@@ -1,57 +1,68 @@
-import React, { useEffect, useState } from 'react';
-import Recipe from './Recipe';
+import React, { useEffect, useState } from "react";
+import Recipe from "./Recipe";
 
-import './App.css';
-import { Container } from 'react-bootstrap';
+import "./App.css";
+import { Container } from "react-bootstrap";
 
 const App = () => {
   const App_ID = "657875ee";
   const App_KEY = "32dac5f3f50243b613e3269f757de49d";
-  // const exampleReq = 
+  // const exampleReq =
   //  `https://api.edamam.com/search?q=chicken&app_id=${ App_ID }&app_key=${App_KEY}`;
   const [recipes, setRecipes] = useState([]);
   const [search, setSearch] = useState("");
-  const [query, setQuery] = useState('chicken');
+  const [query, setQuery] = useState("chicken");
 
-   useEffect(() => {
-     getRecipes();
-   },[query]);
-   const getRecipes = async () =>{
-     const response = await fetch(`https://api.edamam.com/search?q=${query}&app_id=${ App_ID }&app_key=${App_KEY}`);
-     const data = await response.json();
-     setRecipes(data.hits);
-     console.log(data.hits);
-   }
+  useEffect(() => {
+    getRecipes();
+  }, [query]);
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=${query}&app_id=${App_ID}&app_key=${App_KEY}`
+    );
+    const data = await response.json();
+    setRecipes(data.hits);
+    console.log(data.hits);
+  };
 
-   const updateSearch = e =>{
-     setSearch(e.target.value);
-   }
+  const updateSearch = (e) => {
+    setSearch(e.target.value);
+  };
 
-   const getSearch = e =>{
-     e.preventDefault();
-     setQuery(search);
-     setSearch('');
-   }
+  const getSearch = (e) => {
+    e.preventDefault();
+    setQuery(search);
+    setSearch("");
+  };
 
   return (
     <Container>
-    <div className="App">
-      <form onSubmit={getSearch} className="search-form">
-        <input placeholder='Search your favourite recepie' className="search-bar text-center" type="text" value={search} onChange= {updateSearch}/>
-        <button className="search-button" type="submit">Search</button>
-      </form>
-      <div className="recipes">
-      {recipes.map(recipe=>(
-        <Recipe 
-        key = {recipe.recipe.label}
-        title={recipe.recipe.label} 
-        calories={recipe.recipe.calories} 
-        image = {recipe.recipe.image}/>
-      ))}
+      <div className="App">
+        <form onSubmit={getSearch} className="search-form">
+          <input
+            placeholder="Search your favourite recepie"
+            className="search-bar text-center"
+            type="text"
+            value={search}
+            onChange={updateSearch}
+          />
+          <button className="search-button" type="submit">
+            Search
+          </button>
+        </form>
+        <div className="recipes">
+          {recipes.map((recipe) => (
+            <Recipe
+              key={recipe.recipe.label}
+              title={recipe.recipe.label}
+              calories={recipe.recipe.calories}
+              image={recipe.recipe.image}
+            />
+          ))}
+        </div>
       </div>
-    </div>
     </Container>
   );
-}
+};
 
 export default App;
--- src/Recipe.js
+++ src/Recipe.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import { Card, Col } from 'react-bootstrap';
+import React from "react";
+import { Card, Col } from "react-bootstrap";
 
-
-
-
-const Recipe = ({title, calories, image}) =>{
-    return(
-      <Col md={4} className='mt-2 mb-2 ps-4'>
-      <Card style={{ width: '20rem' }}>
-  <Card.Img variant="top" src={image} />
-  <Card.Body>
-    <Card.Title>{title}</Card.Title>
-    <Card.Text>
-    {calories}
-    </Card.Text>
-  </Card.Body>
-</Card>
-</Col>
-     
-    );
-}
+const Recipe = ({ title, calories, image }) => {
+  return (
+    <Col md={4} className="mt-2 mb-2 ps-4">
+      <Card style={{ width: "20rem" }}>
+        <Card.Img variant="top" src={image} />
+        <Card.Body>
+          <Card.Title>{title}</Card.Title>
+          <Card.Text>Calories {calories}</Card.Text>
+        </Card.Body>
+      </Card>
+    </Col>
+  );
+};
 
 export default Recipe;
